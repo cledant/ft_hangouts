1ef18cdff28b8b061ad218e8e51d9d8931b8d890
cledant <cledant@student.42.fr>
2018-06-12 13:14:27 +0200

    added spinner in settings

--- app/src/main/java/fr/cledant/ft_hangouts/SettingsActivity.java
+++ app/src/main/java/fr/cledant/ft_hangouts/SettingsActivity.java
@@ -5,11 +5,15 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity
-		implements View.OnClickListener
+		implements View.OnClickListener, AdapterView.OnItemSelectedListener
 {
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
@@ -28,6 +32,14 @@ public class SettingsActivity extends AppCompatActivity
 		cancel.setOnClickListener(this);
 		TextView save = findViewById(R.id.toolbar_settings_save);
 		save.setOnClickListener(this);
+
+		//Bind Spinner
+		Spinner theme_selection = findViewById(R.id.theme_spinner);
+		ArrayAdapter<CharSequence> theme_adapter = ArrayAdapter.createFromResource(this,
+				R.array.settings_color_choice, android.R.layout.simple_spinner_item);
+		theme_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		theme_selection.setAdapter(theme_adapter);
+		theme_selection.setOnItemSelectedListener(this);
 	}
 
 	@Override
@@ -47,5 +59,36 @@ public class SettingsActivity extends AppCompatActivity
 				break;
 			}
 		}
+	}
+
+	@Override
+	public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+	{
+		switch (position)
+		{
+			case 0:
+			{
+				Snackbar.make(view, "BLUE", Snackbar.LENGTH_LONG)
+						.setAction("Action", null).show();
+				break;
+			}
+			case 1:
+			{
+				Snackbar.make(view, "RED", Snackbar.LENGTH_LONG)
+						.setAction("Action", null).show();
+				break;
+			}
+			case 2:
+			{
+				Snackbar.make(view, "YELLOW", Snackbar.LENGTH_LONG)
+						.setAction("Action", null).show();
+				break;
+			}
+		}
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> parent)
+	{
 	}
 }
