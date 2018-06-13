01816a0dcdc57da160aacad652534be5c0445631
cledant <cledant@student.42.fr>
2018-06-13 14:22:06 +0200

    nav drawer + add button color change with theme selection

--- app/src/main/java/fr/cledant/ft_hangouts/SettingsActivity.java
+++ app/src/main/java/fr/cledant/ft_hangouts/SettingsActivity.java
@@ -1,7 +1,6 @@
 package fr.cledant.ft_hangouts;
 
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
